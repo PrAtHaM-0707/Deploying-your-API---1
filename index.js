2031e8d2b6750eaa54f00408c32a500a6625b303
Pratham Mittal <prathamm4402@gmail.com>
2025-04-07 19:19:06 +0530

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+require('dotenv').config();
+
 const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+
+
+const port = process.env.PORT || 8080;
+
+const apiKey = process.env.API_KEY;
+const serverSecret = process.env.SERVER_SECRET;
+const isKalvian = process.env.IS_KALVIAN === 'true';
+
 
 app.use(express.static('static'));
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
+  console.log('ENV Variables:', { apiKey, serverSecret, isKalvian });
 });
